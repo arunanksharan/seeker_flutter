3556ef7f7222dfad40973e97e4321ab3ee38ff8b
arunanksharan <arunank.sharan@gmail.com>
2025-04-11 23:07:52 +0530

    [log]: adjusted logger level depending on release build

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
 import 'package:seeker/core/presentation/screens/initialization_error_screen.dart';
 
 // Import the generated Firebase options and your App widget
 import 'package:seeker/utils/logger.dart';
 import 'package:seeker/firebase_options.dart'; // Make sure you ran `flutterfire configure`
 import 'package:seeker/app.dart'; // We will create this file next
+import 'package:flutter/foundation.dart'; // Import for kReleaseMode
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (kReleaseMode) {
+    // Release mode: Show only warnings and errors
+    Logger.level = Level.warning;
+    logger.i(
+      "Logger level set to WARNING for release build.",
+    ); // Info won't show in release
+  } else {
+    // Debug mode: Show all logs (or your preferred debug level)
+    Logger.level = Level.debug; // Or Level.trace for most verbose
+    logger.d("Logger level set to DEBUG for debug build.");
+  }
 
   // Initialize Firebase using the generated options
   // This must be done before running the app
